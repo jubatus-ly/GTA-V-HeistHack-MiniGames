21e9abc0a3fea9f499452790c7e7bd562466e7ba
jubatus-ly <jubatus-ly@gmail.com>
2024-11-02 17:50:00 +0100

    added vite + i displayed data from json in html

---
+++ GTA-V-HEISTHACK-MINIGAMES/src/main.ts
@@ -0,0 +1,19 @@
+import "./style.scss";
+import games from "./games.json";
+
+const gs = games.games;
+
+console.log(gs);
+
+document.querySelectorAll("div.card")
+.forEach((element, index) => {
+  const game = gs[index]
+  const headerDiv = element.querySelector("div.card-header") as HTMLDivElement;
+  const contentDiv = element.querySelector("div.card-content") as HTMLDivElement;
+  
+  if(headerDiv && contentDiv) {
+    console.log(headerDiv);
+    headerDiv.textContent = game.title;
+    contentDiv.textContent = game.description;
+  }
+});
