ee4044741761c95237c24e10efe9b59d4ec69264
jubatus-ly <jubatus-ly@gmail.com>
2024-12-23 15:40:32 +0100

    displayed the number and shrinked them and then removed them

--- GTA-V-HEISTHACK-MINIGAMES/src/games/laptop/laptop.ts
+++ GTA-V-HEISTHACK-MINIGAMES/src/games/laptop/laptop.ts
@@ -1,1 +1,107 @@
 import "./laptop.scss";
+
+const puzzleLength = 4;
+const tryAgainButton = document.getElementById('try-again-button');
+const tryAgainElements = document.querySelector('div.center-error-message');
+const containerPuzzle = document.getElementById('container-puzzle');
+tryAgainButton?.addEventListener('click', tryAgain);
+
+const loadingArr = ["etablisement de la connection", "faire des trucs de hackerman...", "code d'accès indiqué; nécessite une saisie captcha humaine..."];
+
+function loadingHack():void {
+    const tryAgainImg = tryAgainElements?.getElementsByTagName('img')[0];
+    const loadingText = tryAgainElements?.getElementsByTagName('h3')[0];
+    tryAgainImg!.src = '../../../asset/laptop/spy.png';
+
+    let arrPromises: Promise<void>[] = [];
+    let promise: Promise<void>;
+
+    for(let i=0; i<loadingArr.length; i++) {
+    
+            promise = new Promise<void>((resolve) => {
+                setTimeout(() => {
+                    if (loadingText) {
+                        loadingText.getElementsByTagName('span')[0].innerHTML = loadingArr[i][0].toUpperCase();
+                        loadingText.childNodes[2].textContent = loadingArr[i].slice(1).toUpperCase();
+                        console.log("changement "+ i);
+                        resolve();
+                    } 
+                }, i * 1000);
+            })
+
+            arrPromises.push(promise);
+        
+    }
+
+
+    Promise.all(arrPromises).then(() => {
+        setTimeout(() => {
+            tryAgainElements?.classList.add('hidden');
+            containerPuzzle?.classList.remove('hidden');
+        }, 1000);
+    });
+
+    hack(puzzleLength);
+
+}
+
+async function tryAgain():Promise<void> {
+    loadingHack();
+
+}
+
+async function init():Promise<void> {
+    loadingHack();
+
+}
+
+function hack(numberSquare: number) {
+    console.log('This is function does the logic of the hacking');
+    const containerPuzzle: HTMLElement = document.getElementById("container-puzzle")!;
+    const arrPuzzleNumber: number[] =  [...Array(puzzleLength)].map((_, a) => a+1);
+    const arrPromisesHideNumbers: Promise<void>[] = [];
+    for (let i=0; i<numberSquare; i++) {
+        const divPuzzle = document.createElement('div');
+        divPuzzle.classList.add('square');
+        const squareNumber = i + 1;
+        divPuzzle.id = "square-" + squareNumber;
+        const index = Math.floor(Math.random() * arrPuzzleNumber.length);
+        const p : Promise<void> = displayNumbers(divPuzzle, arrPuzzleNumber[index], i).then(() => {
+            console.log(`Number ${arrPuzzleNumber[index]} has finished its animation.`);
+        });;
+        arrPromisesHideNumbers.push(p);
+        arrPuzzleNumber.splice(index, 1);
+        containerPuzzle.appendChild(divPuzzle);
+    }
+
+    Promise.all(arrPromisesHideNumbers).then(() => {
+        const squaresElement : NodeListOf<Element> = document.querySelectorAll('.square')!;
+        squaresElement.forEach((value) => {
+            value.querySelector('div')?.remove();
+        });
+        
+        console.log(squaresElement);
+    });
+    
+}
+
+function displayNumbers(divPuzzle: HTMLElement, num: number, index: number): Promise<void> {
+    const numberContainer: HTMLElement = document.createElement('div'); 
+    numberContainer.innerHTML = `${num}`;
+    numberContainer.classList.add('can-shrink');
+    numberContainer.id = `num-${index + 1}`;
+    divPuzzle.appendChild(numberContainer);
+
+    return new Promise<void>((resolve) => 
+        setTimeout(() => {
+            numberContainer.classList.add('number-shrink');
+            // Listen for the transitionend event
+            numberContainer.addEventListener('transitionend', () => {
+                resolve();
+            }, { once: true });
+        }, 4000));
+
+}
+
+init();
+
