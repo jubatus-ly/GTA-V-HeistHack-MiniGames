57395f04a0d1e4c7f541bbf38f46f91e5325c293
jubatus-ly <jubatus-ly@gmail.com>
2025-01-23 16:24:29 +0100

    fix: editing prettierrc file to add semicolons into typescript

--- src/games/laptop/laptop.ts
+++ src/games/laptop/laptop.ts
@@ -1,140 +1,140 @@
-import { sample } from './helpers'
+import { sample } from './helpers';
 import {
   COLORS,
   generateQuestionAndAnswer,
   generateRandomPuzzle,
   PuzzleData,
-} from './laptop-puzzle-factory'
-import './laptop.scss'
-import { getPuzzleSvg } from './svg-factory'
+} from './laptop-puzzle-factory';
+import './laptop.scss';
+import { getPuzzleSvg } from './svg-factory';
 
-const totalPuzzles = 4
+const totalPuzzles = 4;
 const tryAgainContainer: HTMLElement = document.getElementById(
   'try-again-id'
-)! as HTMLElement
+)! as HTMLElement;
 const tryAgainButton: HTMLButtonElement = document.getElementById(
   'try-again-button'
-) as HTMLButtonElement
+) as HTMLButtonElement;
 const centerMessageContainer: HTMLElement = document.querySelector(
   'div.center-message'
-)! as HTMLElement
+)! as HTMLElement;
 const puzzlesContainer: HTMLElement = document.getElementById(
   'container-puzzle'
-)! as HTMLElement
-tryAgainButton?.addEventListener('click', tryAgain)
-const arrGeneratedPuzzle: PuzzleData[] = []
-const arrIndexPuzzleNumber: number[] = []
-let submitted: string | null
-let result: boolean = true
-let answer: string = ''
+)! as HTMLElement;
+tryAgainButton?.addEventListener('click', tryAgain);
+const arrGeneratedPuzzle: PuzzleData[] = [];
+const arrIndexPuzzleNumber: number[] = [];
+let submitted: string | null;
+let result: boolean = true;
+let answer: string = '';
 const loadingArr: string[] = [
   'etablisement de la connection',
   'faire des trucs de hackerman...',
   "code d'accès indiqué; nécessite une saisie captcha humaine...",
-]
-const timerPuzzleSecond: number = 10
+];
+const timerPuzzleSecond: number = 10;
 const answerContainer: HTMLElement =
-  document.getElementById('answer-container')!
-const progressBar: HTMLElement = document.querySelector('div.progress-bar')!
+  document.getElementById('answer-container')!;
+const progressBar: HTMLElement = document.querySelector('div.progress-bar')!;
 const inputPuzzle: HTMLInputElement = document.getElementById(
   'answer-placeholder'
-) as HTMLInputElement
+) as HTMLInputElement;
 const tryAgainImg = centerMessageContainer.getElementsByTagName(
   'img'
-)[0] as HTMLImageElement
+)[0] as HTMLImageElement;
 const loadingText = centerMessageContainer.getElementsByTagName(
   'h3'
-)[0] as HTMLHeadingElement
-const tryAgainHint = document.querySelector('.try-again-hint')! as HTMLElement
+)[0] as HTMLHeadingElement;
+const tryAgainHint = document.querySelector('.try-again-hint')! as HTMLElement;
 
 async function loadingHack(): Promise<void> {
-  tryAgainImg!.src = '../../../asset/laptop/spy.png'
+  tryAgainImg!.src = '../../../asset/laptop/spy.png';
 
-  const arrPromises: Promise<void>[] = []
-  let promise: Promise<void>
+  const arrPromises: Promise<void>[] = [];
+  let promise: Promise<void>;
 
   for (let i = 0; i < loadingArr.length; i++) {
     promise = new Promise<void>((resolve) => {
       setTimeout(() => {
         loadingText.getElementsByTagName('span')[0].innerHTML =
-          loadingArr[i][0].toUpperCase()
+          loadingArr[i][0].toUpperCase();
         loadingText.childNodes[2].textContent = loadingArr[i]
           .slice(1)
-          .toUpperCase()
-        resolve()
-      }, i * 1000)
-    })
+          .toUpperCase();
+        resolve();
+      }, i * 1000);
+    });
 
-    arrPromises.push(promise)
+    arrPromises.push(promise);
   }
 
   Promise.all(arrPromises).then(() => {
     setTimeout(() => {
-      centerMessageContainer?.classList.add('hidden')
-      puzzlesContainer?.classList.remove('hidden')
-    }, 1000)
-  })
+      centerMessageContainer?.classList.add('hidden');
+      puzzlesContainer?.classList.remove('hidden');
+    }, 1000);
+  });
 
-  hack(totalPuzzles)
+  hack(totalPuzzles);
 }
 
 async function tryAgain(): Promise<void> {
-  loadingHack()
+  loadingHack();
 }
 
 async function init(): Promise<void> {
-  loadingHack()
+  loadingHack();
 }
 
 function hack(numberSquares: number): void {
-  result = true
+  result = true;
   if (!tryAgainContainer.classList.contains('hidden'))
-    tryAgainContainer.classList.add('hidden')
-  centerMessageContainer!.classList.remove('margin-top-auto')
+    tryAgainContainer.classList.add('hidden');
+  centerMessageContainer!.classList.remove('margin-top-auto');
   const arrPuzzleNumber: number[] = [...Array(numberSquares)].map(
     (_, a) => a + 1
-  )
-  const arrPromisesHideNumbers: Promise<string>[] = []
+  );
+  const arrPromisesHideNumbers: Promise<string>[] = [];
 
   for (let i = 0; i < numberSquares; i++) {
-    const divPuzzle = document.createElement('div')
-    divPuzzle.classList.add('square')
-    const squareNumber = i + 1
-    divPuzzle.id = 'square-' + squareNumber
-    const index = Math.floor(Math.random() * arrPuzzleNumber.length)
-    arrIndexPuzzleNumber.push(arrPuzzleNumber[index])
+    const divPuzzle = document.createElement('div');
+    divPuzzle.classList.add('square');
+    const squareNumber = i + 1;
+    divPuzzle.id = 'square-' + squareNumber;
+    const index = Math.floor(Math.random() * arrPuzzleNumber.length);
+    arrIndexPuzzleNumber.push(arrPuzzleNumber[index]);
 
-    const puzzleNumber = arrPuzzleNumber[index]
+    const puzzleNumber = arrPuzzleNumber[index];
     const p: Promise<string> = displayNumber(
       divPuzzle,
       arrPuzzleNumber[index],
       i
     ).then(() => {
-      return `Number ${puzzleNumber} has finished its animation.`
-    })
+      return `Number ${puzzleNumber} has finished its animation.`;
+    });
 
-    arrPuzzleNumber.splice(index, 1)
-    arrPromisesHideNumbers.push(p)
-    puzzlesContainer.appendChild(divPuzzle)
+    arrPuzzleNumber.splice(index, 1);
+    arrPromisesHideNumbers.push(p);
+    puzzlesContainer.appendChild(divPuzzle);
   }
 
   Promise.all(arrPromisesHideNumbers).then(async () => {
     const squaresElement: NodeListOf<HTMLElement> =
-      document.querySelectorAll('.square')!
+      document.querySelectorAll('.square')!;
 
     for (let i = 0; i < 3 && result; i++) {
-      ;[submitted, answer] = await displayPuzzles(squaresElement)
-      result = submitted === answer ? true : false
+      [submitted, answer] = await displayPuzzles(squaresElement);
+      result = submitted === answer ? true : false;
     }
 
-    resultPuzzle(result, answer)
+    resultPuzzle(result, answer);
 
     document
       .querySelectorAll('div[id^="square-"]')
-      .forEach((div) => div.remove())
+      .forEach((div) => div.remove());
 
-    return 'Promise all resolved'
-  })
+    return 'Promise all resolved';
+  });
 }
 
 function displayNumber(
@@ -142,50 +142,50 @@ function displayNumber(
   num: number,
   index: number
 ): Promise<string> {
-  const numberContainer: HTMLElement = document.createElement('div')
-  numberContainer.innerHTML = `${num}`
-  numberContainer.classList.add('can-shrink')
-  numberContainer.id = `num-${index + 1}`
-  divPuzzle.appendChild(numberContainer)
+  const numberContainer: HTMLElement = document.createElement('div');
+  numberContainer.innerHTML = `${num}`;
+  numberContainer.classList.add('can-shrink');
+  numberContainer.id = `num-${index + 1}`;
+  divPuzzle.appendChild(numberContainer);
 
   return new Promise<string>((resolve) =>
     setTimeout(() => {
-      numberContainer.classList.add('number-shrink')
+      numberContainer.classList.add('number-shrink');
       // Listen for the transitionend event
       numberContainer.addEventListener(
         'transitionend',
         () => {
-          return resolve('foo')
+          return resolve('foo');
         },
         { once: true }
-      )
+      );
     }, 4000)
-  )
+  );
 }
 
 function counter(timerMs: number): Promise<string | null> {
-  inputPuzzle.value = ''
-  progressBar.style.transition = 'none'
-  progressBar.classList.remove('progress-bar-shrink')
+  inputPuzzle.value = '';
+  progressBar.style.transition = 'none';
+  progressBar.classList.remove('progress-bar-shrink');
 
   requestAnimationFrame(() => {
     requestAnimationFrame(() => {
-      progressBar.style.transition = `width ${timerMs}ms linear`
-      progressBar.classList.add('progress-bar-shrink')
-    })
-  })
+      progressBar.style.transition = `width ${timerMs}ms linear`;
+      progressBar.classList.add('progress-bar-shrink');
+    });
+  });
 
   return new Promise((resolve) => {
     inputPuzzle.addEventListener('keyup', (event) => {
       if (event.key === 'Enter') {
-        resolve(inputPuzzle.value)
+        resolve(inputPuzzle.value);
       }
-    })
+    });
 
     setTimeout(() => {
-      resolve(null)
-    }, timerMs)
-  })
+      resolve(null);
+    }, timerMs);
+  });
 }
 
 function resultPuzzle(result: boolean, answer: string): void {
@@ -194,13 +194,13 @@ function resultPuzzle(result: boolean, answer: string): void {
       '../../../asset/laptop/spy.png',
       'LE SYSTÈME A ÉTÉ CONTOURNÉ.',
       `Bon travail, la réponse était "${answer}"`
-    )
+    );
   } else {
     displayMessageAfterPuzzle(
       '../../../asset/laptop/failed.png',
       "LE SYSTÈME N'A PAS ACCEPTÉ VOS RÉPONSES",
       `Le temps s'est écoulé la réponse était "${answer}"`
-    )
+    );
   }
 }
 
@@ -208,31 +208,31 @@ async function displayPuzzles(
   squaresElement: NodeListOf<HTMLElement>
 ): Promise<[string | null, string]> {
   squaresElement.forEach((value: HTMLElement) => {
-    value.querySelector('div')?.remove()
-    const puzzle = generateRandomPuzzle()
-    arrGeneratedPuzzle.push(puzzle)
-    value.style.background = sample(Object.keys(COLORS))
+    value.querySelector('div')?.remove();
+    const puzzle = generateRandomPuzzle();
+    arrGeneratedPuzzle.push(puzzle);
+    value.style.background = sample(Object.keys(COLORS));
     while (puzzle.colors['background'] === value.style.background)
-      value.style.background = sample(Object.keys(COLORS))
-    const puzzleSVG = getPuzzleSvg(puzzle)
-    value.innerHTML = puzzleSVG
-  })
+      value.style.background = sample(Object.keys(COLORS));
+    const puzzleSVG = getPuzzleSvg(puzzle);
+    value.innerHTML = puzzleSVG;
+  });
 
-  const answerSection = document.getElementById('answer-container')
-  answerSection?.classList.remove('hidden')
+  const answerSection = document.getElementById('answer-container');
+  answerSection?.classList.remove('hidden');
   const [question, answer] = generateQuestionAndAnswer(
     arrIndexPuzzleNumber,
     arrGeneratedPuzzle
-  )
-  const questionDiv = answerSection!.querySelector('.question')!
-  questionDiv.innerHTML = question
-  console.log('Question: ' + question + ' Answer: ' + answer)
+  );
+  const questionDiv = answerSection!.querySelector('.question')!;
+  questionDiv.innerHTML = question;
+  console.log('Question: ' + question + ' Answer: ' + answer);
 
   await counter(timerPuzzleSecond * 1000).then((value) => {
-    submitted = value
-    console.log('Submitted: ' + submitted + ' answer: ' + answer)
-  })
-  return [submitted, answer]
+    submitted = value;
+    console.log('Submitted: ' + submitted + ' answer: ' + answer);
+  });
+  return [submitted, answer];
 }
 
 function displayMessageAfterPuzzle(
@@ -240,37 +240,37 @@ function displayMessageAfterPuzzle(
   message: string,
   hint: string
 ): void {
-  puzzlesContainer.classList.add('hidden')
-  centerMessageContainer!.classList.remove('hidden')
-  answerContainer.classList.add('hidden')
-  tryAgainContainer.classList.remove('hidden')
-  centerMessageContainer!.classList.add('margin-top-auto')
-  tryAgainImg!.src = imageSrc
+  puzzlesContainer.classList.add('hidden');
+  centerMessageContainer!.classList.remove('hidden');
+  answerContainer.classList.add('hidden');
+  tryAgainContainer.classList.remove('hidden');
+  centerMessageContainer!.classList.add('margin-top-auto');
+  tryAgainImg!.src = imageSrc;
   loadingText.getElementsByTagName('span')[0].innerHTML =
-    message[0].toUpperCase()
-  loadingText.childNodes[2].textContent = message.slice(1).toUpperCase()
-  tryAgainHint.innerHTML = hint
+    message[0].toUpperCase();
+  loadingText.childNodes[2].textContent = message.slice(1).toUpperCase();
+  tryAgainHint.innerHTML = hint;
 }
 
-const modal = document.getElementById('modal') as HTMLElement
-const helpButton = document.getElementById('help-button') as HTMLButtonElement
-const closeModalElement = document.getElementById('closeModal') as HTMLElement
+const modal = document.getElementById('modal') as HTMLElement;
+const helpButton = document.getElementById('help-button') as HTMLButtonElement;
+const closeModalElement = document.getElementById('closeModal') as HTMLElement;
 
-helpButton.addEventListener('click', openModal)
-closeModalElement.addEventListener('click', closeModal)
+helpButton.addEventListener('click', openModal);
+closeModalElement.addEventListener('click', closeModal);
 
 function openModal(): void {
-  modal.style.display = 'flex'
+  modal.style.display = 'flex';
 }
 
 function closeModal(): void {
-  modal.style.display = 'none'
+  modal.style.display = 'none';
 }
 
 window.addEventListener('click', (event: MouseEvent) => {
   if (event.target === modal) {
-    closeModal()
+    closeModal();
   }
-})
+});
 
-init()
+init();
